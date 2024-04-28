7beb8985e6e7b9bebca32663f29716c20fd1f8bb
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-04-29 00:03:23 +0700

    Validate everything in a scope and instead of throwing exception just LogWarning.

--- source/TimeWarp.OptionsValidation/Extensions/ServiceProviderExtensions.cs
+++ source/TimeWarp.OptionsValidation/Extensions/ServiceProviderExtensions.cs
@@ -1,46 +1,66 @@
 ﻿namespace Microsoft.Extensions.DependencyInjection;
 
-using FluentValidation;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Logging;
 
 /// <summary>
 /// Run Validation on all the IOptions that have validation
-/// This will itterate through all the IConfigureOptions in the IServiceCollection
+/// This will iterate through all the IConfigureOptions in the IServiceCollection
 /// Then it will access each of those which will trigger the validation.
 /// </summary>
-public static partial class ServiceProviderExtensions
+public static class ServiceProviderExtensions
 {
-  public static void ValidateOptions(this IServiceProvider aServiceProvider, IServiceCollection aServiceCollection)
+  public static void ValidateOptions
+  (
+    this IServiceProvider serviceProvider,
+    IServiceCollection serviceCollection,
+    ILogger logger
+  )
+  {
+    using IServiceScope scope = serviceProvider.CreateScope();
+    IServiceProvider scopedProvider = scope.ServiceProvider;
+    ValidateOptionsInternal(scopedProvider, serviceCollection, logger);
+  }
+
+  private static void ValidateOptionsInternal
+  (
+    this IServiceProvider serviceProvider,
+    IServiceCollection serviceCollection,
+    ILogger logger
+  )
   {
     IEnumerable<Type> optionTypes =
-    aServiceCollection
+    serviceCollection
       .Where
       (
-        aServiceDescriptor =>
-          aServiceDescriptor.ServiceType.IsGenericType &&
-          aServiceDescriptor.ServiceType.GetGenericTypeDefinition() == typeof(IConfigureOptions<>)
+        serviceDescriptor =>
+          serviceDescriptor.ServiceType.IsGenericType &&
+          serviceDescriptor.ServiceType.GetGenericTypeDefinition() == typeof(IConfigureOptions<>)
       )
       .Select
       (
-        aServiceDescriptor => aServiceDescriptor.ServiceType.GetGenericArguments()[0]
+        serviceDescriptor => serviceDescriptor.ServiceType.GetGenericArguments()[0]
       ).Distinct();
 
-    var originalDisplayNameResolver = ValidatorOptions.Global.DisplayNameResolver;
+    Func<Type, MemberInfo, LambdaExpression, string>? originalDisplayNameResolver = ValidatorOptions.Global.DisplayNameResolver;
 
     ValidatorOptions.Global.DisplayNameResolver =
-      (aType, aMemberInfo, aLambdaExpression) =>
-        aType != null && aMemberInfo != null ? $"{aType.Name}:{aMemberInfo.Name}" : null;
+      (type, memberInfo, _) =>
+        type != null && memberInfo != null ? $"{type.Name}:{memberInfo.Name}" : null;
 
 
     foreach (Type optionType in optionTypes)
     {
-      Type optionsAccessorType = typeof(IOptions<>).MakeGenericType(new Type[] { optionType });
-      object? optionsAccessor = aServiceProvider.GetService(optionsAccessorType);
-      // Accessing the value triggers the validation.
-      object? value = optionsAccessor?.GetType().GetProperty(nameof(IOptions<Object>.Value))?.GetValue(optionsAccessor);
+      try
+      {
+        Type optionsAccessorType = typeof(IOptions<>).MakeGenericType(new Type[] { optionType });
+        object? optionsAccessor = serviceProvider.GetService(optionsAccessorType);
+        // Accessing the value triggers the validation.
+        object? _ = optionsAccessor?.GetType().GetProperty(nameof(IOptions<object>.Value))?.GetValue(optionsAccessor);
+      }
+      catch (Exception e)
+      {
+        logger.LogWarning("Failed to validate options for {Name}: {Message}", optionType.Name, e.Message);
+      }
     }
 
     ValidatorOptions.Global.DisplayNameResolver = originalDisplayNameResolver;
