1eeadff127e3e0f43b7c5c859c8eb2f443bc5bfd
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-04-29 00:01:53 +0700

    Move to primary constructor

--- source/TimeWarp.OptionsValidation/Configuration/OptionsValidation.cs
+++ source/TimeWarp.OptionsValidation/Configuration/OptionsValidation.cs
@@ -1,27 +1,16 @@
 ﻿namespace TimeWarp.OptionsValidation;
 
-using FluentValidation;
-using FluentValidation.Results;
-using Microsoft.Extensions.Options;
-using System.Linq;
-
-public class OptionsValidation<TOptions, TOptionsValidator> : IValidateOptions<TOptions>
+public class OptionsValidation<TOptions, TOptionsValidator>
+(
+  TOptionsValidator OptionsValidator
+) : IValidateOptions<TOptions>
   where TOptions : class
   where TOptionsValidator : AbstractValidator<TOptions>
 {
-  private readonly TOptionsValidator OptionsValidator;
 
-  public OptionsValidation
-  (
-    TOptionsValidator aOptionsValidator
-  )
+  public ValidateOptionsResult Validate(string? name, TOptions options)
   {
-    OptionsValidator = aOptionsValidator;
-  }
-
-  public ValidateOptionsResult Validate(string aName, TOptions aOptions)
-  {
-    ValidationResult validationResult = OptionsValidator.Validate(aOptions);
+    ValidationResult validationResult = OptionsValidator.Validate(options);
     if (validationResult.IsValid)
     {
       return ValidateOptionsResult.Success;
@@ -31,6 +20,5 @@ public class OptionsValidation<TOptions, TOptionsValidator> : IValidateOptions<T
     (
       validationResult.Errors.Select(aValidationFailure => aValidationFailure.ErrorMessage)
     );
-
   }
 }
