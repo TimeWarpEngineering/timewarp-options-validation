fb819ac302e327d804a6e27ebb60f44cebc944c7
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-04-29 00:04:36 +0700

    Add GlobalUsings.cs

---
+++ source/TimeWarp.OptionsValidation/GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using FluentValidation;
+global using FluentValidation.Results;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.Extensions.Options;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Linq.Expressions;
+global using System.Reflection;
+global using TimeWarp.OptionsValidation;
